7b8800886c07c52d9d2a61a3a4868530e05e1989
Jordan Millar <jordan.millar@iohk.io>
2021-12-24 15:37:09 -0400

    Implement eligibleLeadershipSlots which returns the slots a given stake pool operator is expected to mint a block.

--- cardano-api/src/Cardano/Api/Shelley.hs
+++ cardano-api/src/Cardano/Api/Shelley.hs
@@ -179,10 +179,15 @@ module Cardano.Api.Shelley
 
     -- ** Local State Query
     DebugLedgerState(..),
+    decodeDebugLedgerState,
     ProtocolState(..),
+    decodeProtocolState,
     SerialisedDebugLedgerState(..),
     UTxO(..),
 
+    -- ** Various calculations
+    LeadershipError(..),
+    eligibleLeadershipSlots,
     -- ** Conversions
     shelleyPayAddrToPlutusPubKHash,
     --TODO: arrange not to export these
@@ -198,6 +203,7 @@ import           Cardano.Api.Eras
 import           Cardano.Api.IPC
 import           Cardano.Api.KeysPraos
 import           Cardano.Api.KeysShelley
+import           Cardano.Api.LedgerState
 import           Cardano.Api.NetworkId
 import           Cardano.Api.OperationalCertificate
 import           Cardano.Api.ProtocolParameters
